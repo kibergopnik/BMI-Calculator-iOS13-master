a5411e2724764862a5e106d4e4a88be868a2f0d4
111 <111>
2020-11-07 20:02:30 +0300

    Новый переименованный ViewController

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
